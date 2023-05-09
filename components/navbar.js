95d19ae46a41ca128e3f6a97b4612c5374fbc7b0
ampahwa <a.pahwa.m@gmail.com>
2023-05-08 23:02:30 -0400

    Added Navbar

---
+++ components/navbar.js
@@ -0,0 +1,63 @@
+import { Component } from "react"
+import { Link } from "react-scroll";
+
+import styles from '../styles/navstyles.module.css'
+
+class NavBar extends Component {
+    render() {
+        return (
+          <div className={styles.App}>
+            <header className={styles.nav}>
+            <nav className={styles.nav__container__actions}>
+            <ul>
+                <li>
+                <Link activeClass={styles.active} smooth spy to="home">
+                    Home
+                </Link>    
+                </li>
+                <li>
+                <Link activeClass={styles.active} smooth spy to="writings">
+                    Writings
+                </Link>
+                </li>
+                <li>
+                <Link activeClass={styles.active} smooth spy to="films">
+                    Films
+                </Link>
+                </li>
+                <li>
+                <Link activeClass={styles.active} smooth spy to="misc">
+                    Misc
+                </Link>
+                </li>
+                <li>
+                <Link activeClass={styles.active} smooth spy to="contact">
+                    Contact
+                </Link>
+                </li>
+            </ul>
+            </nav>
+            </header>
+            <style jsx>{`
+              ul {
+                display: flex;
+                gap: 1rem;
+                font-size: 0.9rem;
+                list-style: none;
+                padding: 0;
+              }
+
+              li {
+                cursor: pointer;
+              }
+
+              li:hover {
+                text-decoration: underline;
+              }
+            `}</style>
+        </div>
+        )
+    }
+}
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,41 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css';
+import NavBar from '../components/navbar';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Austin Barron</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <NavBar />
+      <section id="home" className={utilStyles.home}>
+        <Image
+          src="/../public/AusProfile.jpg" // Route of the image file
+          height={200} // Desired size with correct aspect ratio
+          width={200} // Desired size with correct aspect 
+          className={utilStyles.borderCircle}
+          alt="Austin Barron"
+        />
+        <h1 className={styles.title}> Austin Barron </h1>
+        <h5> A lad to remember! </h5>
+      </section>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <section id="writings" className={utilStyles.writing}>WRITINGS</section>
+      <section id="films" className={utilStyles.film}>FILMS</section>
+      <section id="misc" className={utilStyles.misc}>MISC</section>
+      <section id="contact" className={utilStyles.contact}>CONTACT</section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <p>A Foggy Production</p>
       </footer>
 
       <style jsx>{`
