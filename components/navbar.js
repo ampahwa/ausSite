28c57c346d0e19434a44972c752eaf1a5eb71634
ampahwa <a.pahwa.m@gmail.com>
2023-05-09 01:38:43 -0400

    Trying out new sidebar stuff

--- components/navbar.js
+++ components/navbar.js
@@ -1,63 +1,44 @@
+//LINK DOESN'T WORK INSIDE MENUITEM
+
 import { Component } from "react"
 import { Link } from "react-scroll";
+
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 
 import styles from '../styles/navstyles.module.css'
 
 class NavBar extends Component {
     render() {
         return (
-          <div className={styles.App}>
-            <header className={styles.nav}>
-            <nav className={styles.nav__container__actions}>
-            <ul>
-                <li>
+          <Sidebar>
+            <Menu>
+              <MenuItem>
                 <Link activeClass={styles.active} smooth spy to="home">
                     Home
-                </Link>    
-                </li>
-                <li>
+                </Link>
+              </MenuItem>
+              <MenuItem>
                 <Link activeClass={styles.active} smooth spy to="writings">
                     Writings
                 </Link>
-                </li>
-                <li>
+              </MenuItem>
+              <MenuItem>
                 <Link activeClass={styles.active} smooth spy to="films">
                     Films
                 </Link>
-                </li>
-                <li>
+              </MenuItem>
+              <MenuItem>
                 <Link activeClass={styles.active} smooth spy to="misc">
                     Misc
                 </Link>
-                </li>
-                <li>
+              </MenuItem>
+              <MenuItem>
                 <Link activeClass={styles.active} smooth spy to="contact">
                     Contact
                 </Link>
-                </li>
-            </ul>
-            </nav>
-            </header>
-            <style jsx>{`
-              ul {
-                display:block
-                gap: 1rem;
-                list-style: none;
-                font-size: 24px;
-                padding: 8px
-              }
-
-              li {
-                margin-left: 15px;
-                font-size: 40px;
-                cursor: pointer;
-              }
-
-              li:hover {
-                text-decoration: underline;
-              }
-            `}</style>
-        </div>
+              </MenuItem>
+            </Menu>
+          </Sidebar>
         )
     }
 }
